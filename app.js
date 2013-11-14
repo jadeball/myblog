5dfc017417190be06296d308d25563f3c8a0322a
wangyuqiu <wangyuqiu@gmail.com>
2013-11-14 22:52:38 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by wangyuqiu on 13-11-14.
+ */
+var connect = require('connect');
+connect.createServer(
+    connect.static(__dirname)
+).listen(8888);
---
+++ pages/about-me.md
@@ -0,0 +1,5 @@
+个人介绍
+=======
+<div style="margin: 0 auto 20px;text-align:center;">
+    <img src="../images/mypic.jpeg" alt="avatar">
+</div>
